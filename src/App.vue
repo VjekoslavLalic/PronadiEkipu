8eec860132855806a980c10aa9f64fe38308f667
stela88 <stela88@users.noreply.github.com>
2021-01-03 18:12:53 +0100

    ništa bitno

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,21 @@
 <template>
   <div id="app">
     <nav class="navbar navbar-light bg-light">
-  <div class="container-fluid">
-    <a class="navbar-brand">Odaberi druženje i upoznaj nove ljude!</a> 
-    <form class="d-flex">
-      <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success" type="submit">Search</button>
-    </form>
+      <div class="container-fluid">
+        <a class="navbar-brand">Odaberi druženje i upoznaj nove ljude!</a>
+        <form class="d-flex">
+          <input
+            class="form-control me-2"
+            type="search"
+            placeholder="Search"
+            aria-label="Search"
+          />
+          <button class="btn btn-outline-success" type="submit">Search</button>
+        </form>
+      </div>
+    </nav>
+    <router-view />
   </div>
-</nav>
-    <router-view /></div>
 </template>
 <script>
 import { firebase } from "@/firebase.js";
