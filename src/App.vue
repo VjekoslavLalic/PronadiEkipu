e650e7e095f4a50f70213b5679a1729ee20a4b5c
VjekoslavLalic <VjekoslavLalic@users.noreply.github.com>
2021-01-05 17:12:07 +0100

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,75 @@
 <template>
   <div id="app">
-    <nav class="navbar navbar-light bg-light">
-      <div class="container-fluid">
-        <a class="navbar-brand">Odaberi druženje i upoznaj nove ljude!</a>
-        <form class="d-flex">
-          <input
-            class="form-control me-2"
-            type="search"
-            placeholder="Search"
-            aria-label="Search"
-          />
-          <button class="btn btn-outline-success" type="submit">Search</button>
-        </form>
-      </div>
-    </nav>
     <router-view />
+    <!-- Tabs navs -->
+    <ul class="nav nav-tabs mb-3" id="ex1" role="tablist">
+      <li class="nav-item" role="presentation">
+        <a
+          class="nav-link active"
+          id="ex1-tab-1"
+          data-mdb-toggle="tab"
+          href="#ex1-tabs-1"
+          role="tab"
+          aria-controls="ex1-tabs-1"
+          aria-selected="true"
+          >Druženja</a
+        >
+      </li>
+      <li class="nav-item" role="presentation">
+        <a
+          class="nav-link"
+          id="ex1-tab-2"
+          data-mdb-toggle="tab"
+          href="#ex1-tabs-2"
+          role="tab"
+          aria-controls="ex1-tabs-2"
+          aria-selected="false"
+          >Naslovna</a
+        >
+      </li>
+      <li class="nav-item" role="presentation">
+        <a
+          class="nav-link"
+          id="ex1-tab-3"
+          data-mdb-toggle="tab"
+          href="#ex1-tabs-3"
+          role="tab"
+          aria-controls="ex1-tabs-3"
+          aria-selected="false"
+          >Profil</a
+        >
+      </li>
+    </ul>
+    <!-- Tabs navs -->
+
+    <!-- Tabs content -->
+    <div class="tab-content" id="ex1-content">
+      <div
+        class="tab-pane fade show active"
+        id="ex1-tabs-1"
+        role="tabpanel"
+        aria-labelledby="ex1-tab-1"
+      >
+        Tab 1 content
+      </div>
+      <div
+        class="tab-pane fade"
+        id="ex1-tabs-2"
+        role="tabpanel"
+        aria-labelledby="ex1-tab-2"
+      >
+        Tab 2 content
+      </div>
+      <div
+        class="tab-pane fade"
+        id="ex1-tabs-3"
+        role="tabpanel"
+        aria-labelledby="ex1-tab-3"
+      >
+        Tab 3 content
+      </div>
+    </div>
+    <!-- Tabs content -->
   </div>
 </template>
 <script>
