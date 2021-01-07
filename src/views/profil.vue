f4773e06f2674d0117fbc2c5e5ff4992cfc6917b
VjekoslavLalic <VjekoslavLalic@users.noreply.github.com>
2021-01-08 00:44:07 +0100

    Profil
    Profil

--- src/views/profil.vue
+++ src/views/profil.vue
@@ -1,25 +1,54 @@
 <template>
   <div class="container my-4">
     <!--Grid column-->
-    <div class="col-md-6 mb-4">
+    <div class="profilHeader">
       <form class="md-form">
-        <div class="file-field">
-          <div class="mb-4">
-            <img
-              src="https://mdbootstrap.com/img/Photos/Others/placeholder-avatar.jpg"
-              class="rounded-circle z-depth-1-half avatar-pic"
-              alt="example placeholder avatar"
-            />
-          </div>
-          <div class="d-flex justify-content-center">
-            <div class="btn btn-mdb-color btn-rounded float-left">
-              <input type="file" />
-            </div>
-          </div>
+        <div class="profilSlika">
+          <img
+            src="https://mdbootstrap.com/img/Photos/Others/placeholder-avatar.jpg"
+            class="rounded-circle z-depth-1-half avatar-pic"
+            alt="example placeholder avatar"
+          />
+          <!-- mb-4 -->
         </div>
+
+        <div class="btn btn-mdb-color btn-rounded mb-4">
+          <input type="file" />
+          <!-- btn btn-mdb-color btn-rounded float-left -->
+        </div>
+        <!-- md-form -->
       </form>
+      <!-- col-md-6 mb-4 -->
     </div>
-    <div class="profilForma"></div>
+    <div class="profilForma">
+      <div class="profilPodaci">
+        <div class="profilPodaci1">
+          <p>Gender:</p>
+          <p>E-mail:</p>
+          <p>Number:</p>
+          <p>Age:</p>
+          <!-- / profilPodaci1 -->
+        </div>
+        <div class="profilPodaci2">
+          <p contenteditable="true">Male</p>
+          <p contenteditable="true">lalicvjeko34@gmail.com</p>
+          <p contenteditable="true">0919805009</p>
+          <p contenteditable="true">21</p>
+          <!-- / profilPodaci2 -->
+        </div>
+        <!-- / profilPodaci -->
+      </div>
+      <div class="messengerLogo"><img src="@/assets/messenger.png" /></div>
+      <!-- / profilForma -->
+    </div>
+    <div class="profilOpis">
+      <p>Opis:</p>
+      <textarea
+        contenteditable="true"
+        placeholder="Napišite nešto o sebi."
+      ></textarea>
+    </div>
+    <!-- / container my-4 -->
   </div>
 </template>
 
@@ -54,5 +83,45 @@ export default {
   color: red;
   font-family: monospace;
   font-weight: normal;
+}
+.profilForma {
+  margin: 0px auto;
+  display: flex;
+  max-width: 750px;
+  left: 0;
+  right: 0;
+}
+.profilPodaci {
+  margin: 0px auto;
+  width: 100%;
+  text-align: left;
+  display: flex;
+}
+.profilPodaci1 {
+  margin: 0px 15px 0px 0px;
+}
+.profilPodaci2 {
+  margin: 0px auto 0px 30px;
+}
+
+.messengerLogo {
+  text-align: center;
+  margin: 0px auto 0px 50px;
+  margin-top: 25px;
+}
+.messengerLogo img {
+  width: 100px;
+}
+.profilOpis {
+  text-align: left;
+  max-width: 750px;
+  margin: 0px auto;
+}
+.profilOpis textarea {
+  width: 100%;
+  height: 100px;
+}
+.profilSlika img {
+  margin: 0px auto;
 }
 </style>
