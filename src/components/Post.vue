6187fbfda193a4b36f75781eddf4885c6a63db82
VjekoslavLalic <VjekoslavLalic@users.noreply.github.com>
2021-01-17 16:58:58 +0100

    postovi rade :D
    postovi rade jos trebam sada stavit nekako sliku ali da bude staticna moram skontat da li da ju pusham na firebase ili ne
    mislim da ne ali onda mi nece prikazat idk whyyyyyy

---
+++ src/components/Post.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="card">
+    <div class="card-body p-0">
+      <a :href="info.page"> <img class="card-img-top" :src="info.img" /> </a>
+      <div class="container">
+        <h4>
+          <p>{{ info.img }}</p>
+          <p>{{ info.description }}</p>
+          <p>{{ info.time }}</p>
+        </h4>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["info"],
+  name: "Post",
+};
+</script>
+
+<style>
+.card {
+  /* Add shadows to create the "card" effect */
+  transition: 0.3s;
+  max-width: 750px;
+  left: 0;
+  right: 0;
+  margin: 0px auto 50px auto;
+}
+
+/* On mouse-over, add a deeper shadow */
+.card:hover {
+  box-shadow: 0 8px 16px 0 rgb(255, 255, 255);
+}
+
+/* Add some padding inside the card container */
+.container {
+  padding: 2px 16px;
+}
+</style>
+
--- src/views/Naslovna.vue
+++ src/views/Naslovna.vue
@@ -16,9 +16,11 @@
         <div class="form-box">
           <select class="custom-select" v-model="newPostGame" id="postGame">
             <option selected>Odaberi druženje</option>
-            <option value="1">Čovječe ne ljuti se</option>
-            <option value="2">Uno</option>
-            <option value="3">Belot</option>
+            <option value="Čovječe ne ljuti se">Čovječe ne ljuti se</option>
+            <option value="Uno">Uno</option>
+            <option value="Belot">Belot</option>
+            <option value="Poker">Poker</option>
+            <option value="Briškula">Briškula</option>
           </select>
         </div>
         <div class="form-box">
@@ -32,40 +34,18 @@
           ></textarea>
         </div>
         <div class="form-submit">
-          <input type="submit" value="Publish" />
+          <input type="submit" value="Objavi" id="objavi" />
         </div>
       </form>
     </div>
+    <card v-for="(card, drac) in cards" :key="drac" :info="card" />
   </div>
 </template>
 <script>
-import Card from "@/components/Card.vue";
+import Card from "@/components/Post.vue";
 import { firebase } from "@/firebase.js";
 import store from "@/store";
 import { db } from "@/firebase";
-
-/*cards = [
-  {
-    img: require("@/assets/covjece_ne_ljuti_se.png"),
-    description: "Čovječe ne ljuti se",
-  },
-  { img: require("@/assets/uno.png"), description: "Uno" },
-  { img: require("@/assets/belot.png"), description: "Belot" },
-  { img: require("@/assets/poker.png"), description: "Poker" },
-  { img: require("@/assets/briskula.png"), description: "Briškula" },
-  { img: require("@/assets/kava.png"), description: "Kava" },
-  { img: require("@/assets/setnja.png"), description: "Šetnja" },
-  { img: require("@/assets/trcanje.png"), description: "Trčanje" },
-  {
-    img: require("@/assets/voznja_biciklom.png"),
-    description: "Vožnja biciklom",
-  },
-  { img: require("@/assets/rolanje.png"), description: "Rolanje" },
-  { img: require("@/assets/vani.png"), description: "Vani" },
-  { img: require("@/assets/kod_kuce.png"), description: "Kod kuće" },
-
-  //ovo je statično jer smo ih ručno unijeli ne mijenjaju se tijekom izvođenja applikcijje 
-]; */
 
 export default {
   name: "naslovna",
@@ -75,43 +55,17 @@ export default {
       store,
       newPostOpis: "",
       newPostGame: "",
+      newPostImg: "",
     };
   },
   mounted() {
     this.getPosts();
   },
   methods: {
-    getPosts() {
-      console.log("firebase dohvat...");
-
-      db.collection("posts")
-        .get()
-        .then((query) => {
-          query.forEach((doc) => {
-            this.cards = [];
-            const data = doc.data();
-            console.log(data);
-
-            this.cards.push({
-              id: doc.id,
-              time: data.posted_at,
-              description: data.desc,
-              url: data.url,
-            });
-          });
-        });
-    },
-    //computed: {
-    // filteredCards() {
-    //    let termin = this.store.searchTerm;
-    //  return this.cards.filter((card) => card.description.includes(termin));},},
-
     postNewImage() {
-      // sprema ono sto je korisnik unio u formu
-      // sada kad ih imamo u data možemo ovo popuniti post game = cemu itd
       const postGame = this.newPostGame;
       const postOpis = this.newPostOpis;
-      // i sada je bit da posaljemo varijable na firebase  da bi ostvarili komunikaciju sa firebaseom treba nam db varijabla i moramo ju importati kako bu ju koristili
+      const postImg = this.newPostImg;
 
       db.collection("posts")
         .add({
@@ -119,19 +73,35 @@ export default {
           desc: postOpis,
           email: store.currentUser,
           posted_at: Date.now(),
+          img: postImg,
         })
         .then((doc) => {
-          //funkcija za uspijeh
-          //then je metoda
-          //ako dođe do uspiješnog spremanja :)
-          console.log("Spremljeno", doc); //ako sam dovde dosao znaci sve je uspijesno i onda brisemo sadržaj koji jeu data dijelu sa ovim this.newimagedescription="";
+          console.log("Spremljeno", doc);
           this.newPostGame = "";
           this.newPostOpis = "";
         })
         .catch((e) => {
-          //funkcija za grešku
-          //catch je metoda ona sadrži funkciju za grešku
           console.error(e);
+        });
+    },
+    getPosts() {
+      console.log("firebase dohvat...");
+
+      db.collection("posts")
+        .get()
+        .then((query) => {
+          query.forEach((doc) => {
+            const data = doc.data();
+            console.log(data);
+
+            this.cards.push({
+              id: doc.id,
+              time: data.posted_at,
+              description: data.desc,
+              option: data.option,
+              img: data.postImg,
+            });
+          });
         });
     },
     logout() {
